ec8ffb3059fdf14505145c6d0480825aa6fa39ea
Pavlin Pavlov <888pavlin888@gmail.com>
2020-04-01 20:16:49 +0300

    Finished 5.

---
+++ Expenselt/ExpenseItHome.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Expenselt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Window
+    {
+        public DateTime LastChecked { get; set; }
+
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+            LastChecked = DateTime.Now;
+            this.DataContext = this;
+        }
+
+        public void OpenReport(object sender, RoutedEventArgs e)
+        {
+            ExpenseReport expenseReport = new ExpenseReport(peopleListBox.SelectedItem);
+            expenseReport.Width = this.Width;
+            expenseReport.Height = this.Height;
+            expenseReport.Show();
+        }
+    }
+    
+}
