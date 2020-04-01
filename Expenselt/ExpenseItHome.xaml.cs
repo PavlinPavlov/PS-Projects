4f2f285be76f91b33414e12e83eb538308cf944a
Pavlin Pavlov <888pavlin888@gmail.com>
2020-04-01 22:10:23 +0300

    4th PDF done

--- Expenselt/ExpenseItHome.xaml.cs
+++ Expenselt/ExpenseItHome.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,16 +19,31 @@ namespace Expenselt
     /// <summary>
     /// Interaction logic for ExpenseItHome.xaml
     /// </summary>
-    public partial class ExpenseItHome : Window
+    public partial class ExpenseItHome : Window, INotifyPropertyChanged
     {
-        public DateTime LastChecked { get; set; }
+        private DateTime lastChecked;
+        public DateTime LastChecked
+        {
+            get { return lastChecked; }
+            set
+            {
+                lastChecked = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("LastChecked"));
+            }
+        }
+
+        public ObservableCollection<string> PersonsChecked { get; set; }
 
         public ExpenseItHome()
         {
             InitializeComponent();
             LastChecked = DateTime.Now;
             this.DataContext = this;
+            PersonsChecked = new ObservableCollection<string>();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public void OpenReport(object sender, RoutedEventArgs e)
         {
@@ -35,6 +52,12 @@ namespace Expenselt
             expenseReport.Height = this.Height;
             expenseReport.Show();
         }
+
+        private void peopleListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            LastChecked = DateTime.Now;
+            PersonsChecked.Add((peopleListBox.SelectedItem as System.Xml.XmlElement).Attributes["Name"].Value);
+        }
     }
-    
+
 }
