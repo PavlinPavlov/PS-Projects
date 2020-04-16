b86d4177f1a9ac95a15d7762da0e8349d29517e7
Pavlin Pavlov <888pavlin888@gmail.com>
2020-04-16 20:40:20 +0300

    Implemented form enable/disable with button

---
+++ StudentInfoSystem/ViewModel/BasicCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace StudentInfoSystem.ViewModel
+{
+    class BasicCommand : ICommand
+    {
+        private readonly Action _action;
+        public event EventHandler CanExecuteChanged { add { } remove { } }
+
+        public BasicCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter) { return true; }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ StudentInfoSystem/ViewModel/MainFormVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StudentInfoSystem.ViewModel
+{
+    class MainFormVM : INotifyPropertyChanged
+    {
+        private readonly Student _studentProp;
+        public Student StudentProp { get { Student s = new Student(); s.Degree = "Test"; return s;  } set { } }
+        public bool IsEnabled { get; set; }
+
+        // from INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand EnableCommand
+        {
+            get { return new BasicCommand(SwapIsEnabled); }
+        }
+
+        private void SwapIsEnabled()
+        {
+            IsEnabled = !IsEnabled;
+            PropertyChanged(this, new PropertyChangedEventArgs("IsEnabled"));
+        }
+
+    }
+
+}
